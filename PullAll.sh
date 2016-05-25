01ee340d931e1f0c5c3736074f3cfb6d4db91469
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2016-05-26 00:01:51 +0200

    [PullAll.sh]Corretto errore

--- PullAll.sh
+++ PullAll.sh
@@ -4,12 +4,12 @@ cd Chat/ChatClient && git pull origin master && cd src && git pull origin master
 echo ""
 echo "Aggiorno progetto ChatServer"
 echo "----------------------------"
-cd Chat/ChatServer && git pull origin masterr && cd src && git pull origin master
+cd ../../../Chat/ChatServer && git pull origin masterr && cd src && git pull origin master
 echo ""
 echo "Aggiorno progetto ChatUtils"
 echo "----------------------------"
-cd Chat/ChatUtils && git pull origin masterr && cd src && git pull origin master
+cd ../../../Chat/ChatUtils && git pull origin masterr && cd src && git pull origin master
 echo ""
 echo "Aggiorno progetto Utils"
 echo "-----------------------"
-cd Chat/Utils && git pull origin masterr && cd src && git pull origin master
+cd ../../../Chat/Utils && git pull origin masterr && cd src && git pull origin master
