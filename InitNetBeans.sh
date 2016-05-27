fc939a882cdad2b441a1a9588df8028b870825b4
Edoardo Zanoni <Ryther@users.noreply.github.com>
2016-05-27 18:33:18 +0200

    [InitNetBeans.sh]Aggiornato repository a ZanoBerta

--- InitNetBeans.sh
+++ InitNetBeans.sh
@@ -8,14 +8,14 @@ cd Chat
 echo ""
 echo "Clono i progetti ed i sorgenti"
 echo "------------------------------"
-git clone https://github.com/Ryther/Scuola_ChatClientProject.git ChatClient
-git clone https://github.com/Ryther/Scuola_ChatClient.git ChatClient/src/.
-git clone https://github.com/Ryther/Scuola_ChatServerProject.git ChatServer
-git clone https://github.com/Ryther/Scuola_ChatServer.git ChatServer/src/.
-git clone https://github.com/Ryther/Scuola_ChatUtilsProject.git ChatUtils
-git clone https://github.com/Ryther/Scuola_ChatUtils.git ChatUtils/src/.
-git clone https://github.com/Ryther/Scuola_UtilsProject.git Utils
-git clone https://github.com/Ryther/Scuola_Utils.git Utils/src/.
+git clone https://github.com/ZanoBerta/Scuola_ChatClientProject.git ChatClient
+git clone https://github.com/ZanoBerta/Scuola_ChatClient.git ChatClient/src/.
+git clone https://github.com/ZanoBerta/Scuola_ChatServerProject.git ChatServer
+git clone https://github.com/ZanoBerta/Scuola_ChatServer.git ChatServer/src/.
+git clone https://github.com/ZanoBerta/Scuola_ChatUtilsProject.git ChatUtils
+git clone https://github.com/ZanoBerta/Scuola_ChatUtils.git ChatUtils/src/.
+git clone https://github.com/ZanoBerta/Scuola_UtilsProject.git Utils
+git clone https://github.com/ZanoBerta/Scuola_Utils.git Utils/src/.
 echo ""
 echo "Torno alla cartella di esecuzione"
 echo "---------------------------------"
