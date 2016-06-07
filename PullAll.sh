24f8c5432fada3c77e059bc5336f21879ba747c5
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2016-06-07 12:04:47 +0200

    [PullAll.sh]Modificato pull per ottenere tutti i branch

--- PullAll.sh
+++ PullAll.sh
@@ -1,15 +1,15 @@
 echo "Aggiorno progetto ChatClient"
 echo "----------------------------"
-cd Chat/ChatClient && git fetch --all && git reset --hard origin/master && cd src && git pull origin master
+cd Chat/ChatClient && git fetch --all && git reset --hard origin/master && cd src && git pull
 echo ""
 echo "Aggiorno progetto ChatServer"
 echo "----------------------------"
-cd ../../../Chat/ChatServer && git fetch --all && git reset --hard origin/master && cd src && git pull origin master
+cd ../../../Chat/ChatServer && git fetch --all && git reset --hard origin/master && cd src && git pull
 echo ""
 echo "Aggiorno progetto ChatUtils"
 echo "----------------------------"
-cd ../../../Chat/ChatUtils && git fetch --all && git reset --hard origin/master && cd src && git pull origin master
+cd ../../../Chat/ChatUtils && git fetch --all && git reset --hard origin/master && cd src && git pull
 echo ""
 echo "Aggiorno progetto Utils"
 echo "-----------------------"
-cd ../../../Chat/Utils && git fetch --all && git reset --hard origin/master && cd src && git pull origin master
+cd ../../../Chat/Utils && git fetch --all && git reset --hard origin/master && cd src && git pull
